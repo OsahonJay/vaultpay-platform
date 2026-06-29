c2d5d211fa3653e6599ac8c529564f24e9fe0c13
Osahon Seth I <68052439+OsahonJay@users.noreply.github.com>
2026-06-29 09:39:09 +0100

    Add S3 backend configuration for Terraform

---
+++ terraform/environments/dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "vaultpay-terraform-state-dev"
+    key            = "dev/terraform.tfstate"
+    region         = "eu-west-2"
+    encrypt        = true
+    dynamodb_table = "vaultpay-terraform-locks-dev"
+  }
+}
