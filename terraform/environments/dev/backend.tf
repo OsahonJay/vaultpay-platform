92854aa0154ed6484114caddde1e7b640c1ee785
Osahon Seth I <68052439+OsahonJay@users.noreply.github.com>
2026-06-29 10:07:51 +0100

    Change S3 key path for Terraform state

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "vaultpay-terraform-state-dev"
-    key            = "dev/terraform.tfstate"
+    key            = "dev/networking/terraform.tfstate"
     region         = "eu-west-2"
     encrypt        = true
     dynamodb_table = "vaultpay-terraform-locks-dev"
