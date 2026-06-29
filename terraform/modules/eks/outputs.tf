e418896665320827fa7d1476b370423c8ee5cad9
Osahon Seth I <68052439+OsahonJay@users.noreply.github.com>
2026-06-29 11:21:04 +0100

    Add outputs for EKS cluster details

---
+++ terraform/modules/eks/outputs.tf
@@ -0,0 +1,19 @@
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.main.name
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_oidc_issuer_url" {
+  description = "OIDC issuer URL for IRSA"
+  value       = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+output "oidc_provider_arn" {
+  description = "OIDC provider ARN for IRSA"
+  value       = aws_iam_openid_connect_provider.eks.arn
+}
