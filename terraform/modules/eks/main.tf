5410f060a5501fa10287fbd47bfe14129d9d2aa4
OsahonJay <osahonjustine@gmail.com>
2026-06-30 15:56:22 +0100

    fix: suppress Semgrep eks-public-endpoint-enabled finding - accepted risk for dev, matches CKV_AWS_38/39 exceptions

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -91,7 +91,7 @@ resource "aws_eks_cluster" "main" {
   vpc_config {
     subnet_ids              = var.private_subnet_ids
     endpoint_private_access = true
-    endpoint_public_access  = true
+    endpoint_public_access  = true # nosemgrep: terraform.lang.security.eks-public-endpoint-enabled.eks-public-endpoint-enabled
     public_access_cidrs     = var.allowed_cidr_blocks
   }
 
